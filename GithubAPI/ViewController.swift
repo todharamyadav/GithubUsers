de15028ace6476a00ad39e7845a5c95ee46d0263
Dharamvir <dharamvir@dharamvirs-macbook-air.local>
2016-09-20 13:03:02 -0400

    Initial Commit

---
+++ GithubAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GithubAPI
+//
+//  Created by Dharamvir on 9/20/16.
+//  Copyright © 2016 Dharamvir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
